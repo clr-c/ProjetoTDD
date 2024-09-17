a9ec72334ae04b9b54aad3a81bc11a0618f5ff2e
clr-c <cbaraldic@gmail.com>
2024-09-16 21:31:29 -0300

    Update Program.cs

--- Program/Program.cs
+++ Program/Program.cs
@@ -4,7 +4,6 @@ namespace ProjetoTDD.Program
 {
     public class Programa
     {
-        //Chamando o método para calcular e exibir o resultado
         public static void Main()
         {
             //Chamando o método para calcular e exibir o resultado
